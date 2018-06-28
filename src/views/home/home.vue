c78e5769fde0d4532d1fc58ecc3098d1506e4e82
chenwenquan <852501109@qq.com>
2018-06-28 16:16:28 +0800

    login pag

---
+++ src/views/home/home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h3>欢迎 {{name}}</h3>
+    <a href="#" @click="quit">注销登录</a>
+  </div>
+</template>
+<script>
+import { setCookie,getCookie,delCookie } from '../../assets/js/cookie.js'
+export default {
+  name: 'Home',
+  data () {
+    return {
+      name:'',
+      msg: 'Welcome to Home'
+    }
+  },
+  mounted(){
+    let uname = getCookie('username')
+    this.name = uname
+    if(uname == ""){
+      this.$router.push('/')
+    }
+  },
+  methods:{
+      quit(){
+          delCookie('username')
+      }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/main/main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main">
+  </div>
+</template>
+<script>
+export default {
+  name: 'Main',
+  data () {
+    return {
+      name:'Main',
+      msg: 'Welcome to Main'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
