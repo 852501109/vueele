398ea6a41e90b17ea24ec7e9f4322d7c3f8ff497
lindong <dld0813@163.com>
2018-07-04 23:36:08 +0800

    add vuex

---
+++ src/store/actions.js
@@ -0,0 +1,13 @@
+/**
+ * Create By Ding on  2018/6/28
+ */
+
+
+const actions = {
+    addNum({commit,state},item){
+        commit('ADD_ITEMNUM',1)
+    }
+};
+
+export default actions;
+
---
+++ src/store/getters.js
@@ -0,0 +1,9 @@
+/**
+ * Create By Ding on  2018/6/28
+ */
+
+const getters = {
+
+};
+
+export default getters;
